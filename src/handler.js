8c09ffd50ea3c058c48f40bac81deb2cb021f6ca
Marccel <nengahyuichiro34@gmail.com>
2023-04-29 21:03:13 +0700

    Mengubah pesan delete

--- src/handler.js
+++ src/handler.js
@@ -25,7 +25,7 @@ const addNoteHandler = (request, h) => {
       status: "success",
       message: "Catatan berhasil ditambahkan",
       data: {
-        noteID: id,
+        noteId: id,
       },
     });
     response.code(201);
@@ -100,27 +100,27 @@ const editNoteByIdHandler = (request, h) => {
 };
 
 //fungsi delete
-const deleteNoteByIdHandler = (request,h) => {
-  const {id} = request.params;
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
 
   const index = notes.findIndex((note) => note.id === id);
 
-  if(index !== -1){
-    notes.splice(index,1);
+  if (index !== -1) {
+    notes.splice(index, 1);
 
     const response = h.response({
-      status : "success",
-      message : "Catatan berhasil dihapus",
+      status: "success",
+      message: "Catatan berhasil dihapus",
     });
     response.code(200);
     return response;
   }
   const response = h.response({
-    status : "fail",
-    message : "Catatan gagal dihapus. Id tidak ditemukan",
+    status: "fail",
+    message: "Catatan tidak ditemukan",
   });
   response.code(404);
   return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,deleteNoteByIdHandler};
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
